04844b0467c2055397ffa46236905b3dad1e1fcb
Yoovin Shim <yoovin@Yoovin-MacBookPro.local>
2019-10-20 17:26:15 +0900

    Google ClientId 모듈화

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.scss';
 import Todolist from './component/Todolist'
 import Todoslide from './component/Todoslide';
 import Headforanimate from './component/Headforanimate'
+import Googlelogin from 'react-google-login'
+import {GoogleLogout} from 'react-google-login'
+import googleClientTemp from './googleClientTemp.json'
 
 class App extends React.Component{
 
@@ -11,7 +14,9 @@ constructor(props){
   this.state = {
     todo:'',
     menu:true,
-    user:'user1'
+    user:null,
+    name:null,
+    login:false
   }
 }
 
@@ -30,27 +35,73 @@ callApi = async () => {
 
 selectMenu = () => {
   !this.state.menu ? this.setState({menu:true}) : this.setState({menu:false})
+}
 
+responseGoogle = (res) => {
+  console.log(res)
+  window.sessionStorage.setItem('user', res.googleId)
+  window.sessionStorage.setItem('name', res.profileObj.name)
+  window.sessionStorage.setItem('login', true)
+  this.setState({
+    user: window.sessionStorage.getItem('user'),
+    name: window.sessionStorage.getItem('name'),
+    login: true
+})
+  this.stateRefresh()
+}
+
+logout = () => {
+  // window.sessionStorage.clear()
+  this.setState({login:false})
+}
+
+responseFail = (err) => {
+  console.log(err)
 }
 
 componentDidMount(){
+  this.setState({user: window.sessionStorage.getItem('user')})
+  this.setState({name: window.sessionStorage.getItem('name')})
+  this.setState({login: window.sessionStorage.getItem('login')})
 
-  this.callApi()
-  .then(res => this.setState({todo:res}))
-  .catch(err => console.log(err))
+  console.log(this.state)
+  
+  this.stateRefresh()
 
 }
 
   render(){
-    return(
-      
-      <body>
-        <Headforanimate/>
-        <Todoslide menu={this.state.menu} selectMenu={this.selectMenu}/>
-        <h1 className="title">ㅌㄷ</h1>
-        <Todolist user={this.state.user} todo={this.state.todo} menu={this.state.menu} stateRefresh={this.stateRefresh}/>
-      </body>
-    )
+    if(this.state.login){
+      return(
+        <body>
+          <Headforanimate/>
+          <GoogleLogout
+            clientId={googleClientTemp.client_id}
+            buttonText="Logout"
+            onLogoutSuccess={this.logout}
+            onFailure={this.responseFail}
+          ></GoogleLogout>
+          <Todoslide menu={this.state.menu} selectMenu={this.selectMenu}/>
+          <h1 className="title">{this.state.name}의 해야할일</h1>
+          <Todolist user={this.state.user} 
+          todo={this.state.todo} 
+          menu={this.state.menu} 
+          stateRefresh={this.stateRefresh}
+          />
+        </body>
+        )
+    }else {
+      return(
+        <Googlelogin
+          clientId = {googleClientTemp.client_id}
+          buttonText = "Sign in with Google"
+          onSuccess = {this.responseGoogle}
+          onFailure = {this.responseFail}
+          coockePolicy={'single_host_origin'}
+      /> )
+    }
+    
+    
   }
 }
 
