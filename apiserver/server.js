94ad62bfc48d6cba375e2a69887b7d0809dc82ca
Yoovin Shim <yoovin@Yoovin-MacBookPro.local>
2019-10-20 17:20:17 +0900

    Google 로그인 기능 추가

--- apiserver/server.js
+++ apiserver/server.js
@@ -8,49 +8,59 @@ const adapter = new FileSync('tododb.json')
 const db = low(adapter)
 const shortid = require('shortid')
 
-db.defaults({user1:[], user2:[]}).write()
-// db.set("user3", []).write() 새로 테이블 만들기
+// db.defaults({}).write()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+// Routers
 
-app.get('/api/todo/:user', (req, res)=>{ // read
+// Read
+app.get('/api/todo/:user', (req, res) => { 
     var user = req.params.user
     var todos = db.get(`${user}`).value()
     res.json(todos)
+    // res.send(todos)
 })
 
-app.post('/api/todo', (req, res) => { // add
+// Create
+app.post('/api/todo', (req, res) => { 
 
-  if(!req.body.content){
+  var user = req.body.user
+
+  if(!req.body.content || user === null){
     console.log('todo add에서 404로 응답했음!')
   return res.status(400).send()
   }
 
+  if(db.get(user).value() === undefined){
+    db.set(user, []).write()
+  }
   var newId = shortid.generate()
-  db.get(`${req.body.user}`)
+  db.get(user)
   .push({
     id: newId,
     content:req.body.content,
     isdone:false
   }).write()
-  console.log(`id ${newId} content:${req.body.content} 추가`)
+  console.log(`user:${req.body.user} id:${newId} content:${req.body.content} 추가`)
 
   return res.status(200).send()
 })
 
-app.post('/api/todo/delete/', (req, res)=>{ //delete
+//Delete
+app.post('/api/todo/delete/', (req, res)=>{ 
   var reqId = req.body.id
   db.get(`${req.body.user}`)
     .remove({id:reqId})
     .write()
-  console.log(`id: ${reqId} 삭제`)
+  console.log(`user:${req.body.user} id: ${reqId} 삭제`)
   return res.status(200).send()
   
 })
 
-app.post('/api/todo/update', (req, res)=>{ //update
+//Update
+app.post('/api/todo/update', (req, res)=>{ 
   var reqId = req.body.id
   var reqContent = req.body.content
   db.get(`${req.body.user}`)
@@ -58,20 +68,22 @@ app.post('/api/todo/update', (req, res)=>{ //update
     .assign({content:reqContent})
     .write()
 
-    console.log(`id: ${reqId} Content:${reqContent} 수정`)
+    console.log(`user:${req.body.user} id: ${reqId} Content:${reqContent} 수정`)
     return res.status(200).send()
   }
 )
 
-app.post('/api/todo/isdone', (req, res) => { //isdone 
+//Donecheck 
+app.post('/api/todo/isdone', (req, res) => { 
   var reqId = req.body.id
   var reqDone = req.body.isdone
   db.get(`${req.body.user}`)
     .find({id:reqId})
     .assign({isdone:reqDone})
     .write()
-    console.log(`id: ${reqId} ${reqDone.toString()} Checked`)
+    console.log(`user:${req.body.user} id: ${reqId} ${reqDone.toString()} Checked`)
     return res.status(200).send()
     }
 )
+
 app.listen(port, ()=> console.log(`API 서버가 ${port}에서 돌아가는중!`))
